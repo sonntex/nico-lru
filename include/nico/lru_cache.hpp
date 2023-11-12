d976764a8854a500e36cddafd568134efd3df802
Nikolay Beloborodov <sonntex@gmail.com>
2023-11-12 18:20:22 +0000

    1

--- include/nico/lru_cache.hpp
+++ include/nico/lru_cache.hpp
@@ -12,21 +12,113 @@ template <
     typename T,
     typename Allocator = std::allocator<Key>
   >
-class lru_cache
+class lru_cache1
 {
 public:
 
   using key_type = Key;
   using mapped_type = T;
   using allocator_type = Allocator;
-  using list_type = std::list<key_type, allocator_type>;
+  using list_type = std::list<key_type>;
   using map_type = std::unordered_map<key_type, std::pair<mapped_type, typename list_type::iterator>>;
   using size_type = typename map_type::size_type;
 
-  lru_cache(size_type capacity, const allocator_type& allocator = {})
+  lru_cache1(size_type capacity, const allocator_type& allocator = {})
     : capacity_{capacity}
-    , list_{allocator}
   {
+    // map_.reserve(capacity);
+  }
+
+  size_type capacity() const { return capacity_; }
+  size_type size() const { return map_.size(); }
+  bool empty() const { return map_.empty(); }
+
+  bool contains(const key_type& key)
+  {
+    return map_.contains(key);
+  }
+
+  void clear()
+  {
+    map_.clear();
+    list_.clear();
+  }
+
+  void insert(const key_type& key, const mapped_type& value)
+  {
+    auto i = map_.find(key);
+    if (i == map_.end()) {
+      if (size() >= capacity()) {
+        evict();
+      }
+      list_.push_front(key);
+      map_[key] = std::make_pair(value, list_.begin());
+    } else {
+      auto& [old_value, j] = i->second;
+      if (j != list_.begin()) {
+        list_.erase(j);
+        list_.push_front(key);
+        j = list_.begin();
+      }
+      old_value = value;
+    }
+  }
+
+  std::optional<mapped_type> get(const key_type& key)
+  {
+    auto i = map_.find(key);
+    if (i == map_.end()) {
+      return {};
+    }
+    auto& [value, j] = i->second;
+    if (j != list_.begin()) {
+      list_.erase(j);
+      list_.push_front(key);
+      j = list_.begin();
+    }
+    return value;
+  }
+
+private:
+
+  void evict()
+  {
+    map_.erase(list_.back());
+    list_.pop_back();;
+  }
+
+  size_type capacity_;
+  list_type list_;
+  map_type map_;
+};
+
+template <
+    typename Key,
+    typename T,
+    typename Allocator = std::allocator<Key>
+  >
+class lru_cache2
+{
+public:
+
+  using key_type = Key;
+  using mapped_type = T;
+  using allocator_type = Allocator;
+  using list_type = std::pmr::list<key_type>;
+  using map_type = std::pmr::unordered_map<key_type, std::pair<mapped_type, typename list_type::iterator>>;
+  using size_type = typename map_type::size_type;
+
+  std::pmr::polymorphic_allocator<Key> pa1;
+  std::pmr::polymorphic_allocator<std::pair<key_type, std::pair<mapped_type, typename list_type::iterator>>> pa2;
+
+  lru_cache2(size_type capacity, std::pmr::monotonic_buffer_resource& mr1, std::pmr::monotonic_buffer_resource& mr2, const allocator_type& allocator = {})
+    : capacity_{capacity}
+    , pa1{&mr1}
+    , list_{pa1}
+    , pa2{&mr2}
+    , map_{pa2}
+  {
+    // map_.reserve(capacity);
   }
 
   size_type capacity() const { return capacity_; }
--- performance/performance.cpp
+++ performance/performance.cpp
@@ -7,8 +7,11 @@
 
 #include <nico/lru_cache.hpp>
 
-constexpr size_t max_n() { return 1024 * 1024 * 100; }
-constexpr size_t max_capacity() { return 1024; }
+// constexpr int iterations{1000};
+constexpr int total_nodes{10'000'000};
+
+constexpr size_t max_n() { return total_nodes; }
+constexpr size_t max_capacity() { return 10000; }
 
 std::vector<std::pair<uint32_t, std::string>> generate_n(size_t n)
 {
@@ -25,24 +28,133 @@ std::vector<std::pair<uint32_t, std::string>> generate_n(size_t n)
 
 const auto values = generate_n(max_n());
 
-BOOST_AUTO_TEST_CASE(test_lru_std)
+// BOOST_AUTO_TEST_CASE(test_lru_std)
+// {
+//   using lru_cache = nico::lru_cache1<uint32_t, std::string>;
+//   lru_cache cache{max_capacity()};
+//   for (const auto& [key, value] : values) {
+//     cache.insert(key, value);
+//   }
+// }
+
+// std::array<char, max_capacity() * 32> buf; // enough to fit in all nodes
+// std::pmr::monotonic_buffer_resource mr{buf.data(), buf.size()};
+
+// BOOST_AUTO_TEST_CASE(test_lru_pmr)
+// {
+//   using lru_cache = nico::lru_cache2<uint32_t, std::string, std::pmr::polymorphic_allocator<uint32_t>>;
+//   std::pmr::polymorphic_allocator<int> pa{&mr};
+//   lru_cache cache{max_capacity(), pa};
+//   for (const auto& [key, value] : values) {
+//     cache.insert(key, value);
+//   }
+// }
+
+// BOOST_AUTO_TEST_CASE(test_list_std)
+// {
+//   for (int i = 0; i < 100; ++i) {
+//     std::list<uint32_t> list;
+//     for (const auto& [key, value] : values) {
+//       list.push_back(key);
+//     }
+//   }
+// }
+
+// BOOST_AUTO_TEST_CASE(test_list_pmr)
+// {
+//   for (int i = 0; i < 100; ++i) {
+//     std::pmr::list<uint32_t> list;
+//     for (const auto& [key, value] : values) {
+//       list.push_back(key);
+//     }
+//   }
+// }
+
+// template<typename Func>
+// void benchmark(Func test_func, int iterations)
+// {
+//   while (iterations-- > 0)
+//     test_func();
+// }
+
+// auto default_std_alloc = [total_nodes]
+//  {
+//    std::list<int> list;
+//    for (const auto& [key, value] : values)
+//      list.push_back(key);
+//  };
+//  
+// auto default_pmr_alloc = [total_nodes]
+//  {
+//    std::pmr::list<int> list;
+//    for (const auto& [key, value] : values)
+//      list.push_back(key);
+//  };
+//  
+// auto pmr_alloc_no_buf = [total_nodes]
+//  {
+//    std::pmr::monotonic_buffer_resource mbr;
+//    std::pmr::polymorphic_allocator<int> pa{&mbr};
+//    std::pmr::list<int> list{pa};
+//    for (const auto& [key, value] : values)
+//      list.push_back(key);
+//  };
+//  
+// auto pmr_alloc_and_buf = [total_nodes]
+//  {
+//    std::array<std::byte, total_nodes * 32> buffer; // enough to fit in all nodes
+//    std::pmr::monotonic_buffer_resource mbr{buffer.data(), buffer.size()};
+//    std::pmr::polymorphic_allocator<int> pa{&mbr};
+//    std::pmr::list<int> list{pa};
+//    for (const auto& [key, value] : values)
+//      list.push_back(key);
+//  };
+
+auto default_std_alloc = []
+ {
+   using lru_cache = nico::lru_cache1<uint32_t, int>;
+   lru_cache cache{max_capacity()};
+   for (const auto& [key, value] : values)
+     cache.insert(key, key);
+ };
+
+std::pmr::monotonic_buffer_resource mr1;
+std::pmr::monotonic_buffer_resource mr2;
+
+auto pmr_alloc_no_buf = []
+ {
+   using lru_cache = nico::lru_cache2<uint32_t, int>;
+   lru_cache cache{max_capacity(), mr1, mr2};
+   for (const auto& [key, value] : values)
+     cache.insert(key, key);
+ };
+
+std::array<std::byte, max_capacity() * 1024> buf3;
+std::pmr::monotonic_buffer_resource mr3{buf3.data(), buf3.size()};
+std::array<std::byte, max_capacity() * 1024> buf4;
+std::pmr::monotonic_buffer_resource mr4{buf4.data(), buf4.size()};
+
+auto pmr_alloc_and_buf = []
+ {
+   using lru_cache = nico::lru_cache2<uint32_t, int>;
+   lru_cache cache{max_capacity(), mr3, mr4};
+   for (const auto& [key, value] : values)
+     cache.insert(key, key);
+ };
+
+BOOST_AUTO_TEST_CASE(test_std_alloc)
 {
-  using lru_cache = nico::lru_cache<uint32_t, std::string>;
-  lru_cache cache{max_capacity()};
-  for (const auto& [key, value] : values) {
-    cache.insert(key, value);
-  }
+  default_std_alloc();
 }
-
-std::array<char, max_capacity() * 32> buf; // enough to fit in all nodes
-std::pmr::monotonic_buffer_resource mr{buf.data(), buf.size()};
-
-BOOST_AUTO_TEST_CASE(test_lru_pmr)
+// BOOST_AUTO_TEST_CASE(test_pmr_alloc)
+// {
+//   benchmark(default_pmr_alloc, iterations);
+// }
+BOOST_AUTO_TEST_CASE(test_pmr_alloc_no_buf)
 {
-  using lru_cache = nico::lru_cache<uint32_t, std::string, std::pmr::polymorphic_allocator<uint32_t>>;
-  std::pmr::polymorphic_allocator<int> pa{&mr};
-  lru_cache cache{max_capacity(), pa};
-  for (const auto& [key, value] : values) {
-    cache.insert(key, value);
-  }
+  pmr_alloc_no_buf();
+}
+BOOST_AUTO_TEST_CASE(test_pmr_alloc_and_buf)
+{
+  pmr_alloc_and_buf();
 }
